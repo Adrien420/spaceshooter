6913e867b89e8a6208532ce3a77e378ae9ad0abe
Adrien420 <114665583+Adrien420@users.noreply.github.com>
2023-08-24 17:36:44 +0200

    Update firebase_google.js

--- script/firebase_google.js
+++ script/firebase_google.js
@@ -7,7 +7,7 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, setPersistence, 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
+    apiKey: "AIzaSyAByHFKlgTGylA4QYKJMC9VRHMk4O6-6XI",
     authDomain: "spaceshooter-adproject.firebaseapp.com",
     databaseURL: "https://spaceshooter-adproject-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "spaceshooter-adproject",
